e0d7477843e010718a1e5631f190714b76da0103
JasonRucinski <31479926+JasonRucinski@users.noreply.github.com>
2023-07-18 12:45:17 -0500

    Update wizard.py

--- wizard.py
+++ wizard.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext import commands
+from gamedaybot.espn.functionality import *
 import openai
 import os
 from dotenv import load_dotenv
@@ -6,32 +8,11 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Set up the Discord client
-client = discord.Client()
-
 # Set up the OpenAI API key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Define a function to generate responses using GPT-3
-def generate_response(prompt):
-    # Set up the GPT-3 parameters
-    model_engine = "davinci" # Or any other GPT-3 engine you have access to
-    prompt = f"{prompt}\nAI:"
+bot = commands.Bot(command_prefix='!')
 
-    # Call the OpenAI API to generate a response
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=60,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-
-    # Extract the response text from the API response
-    response_text = response.choices[0].text.strip()
-
-    return response_text
 
 # Define a function to handle Discord messages
 @client.event
@@ -40,12 +21,109 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # Generate a response using GPT-3
-    prompt = message.content
-    response = generate_response(prompt)
+  import discord
+from discord.ext import commands
+from gamedaybot.espn.functionality import *
 
-    # Send the response back to the same channel
-    await message.channel.send(response)
+bot = commands.Bot(command_prefix='!')
 
-# Run the Discord client
-client.run(os.environ.get("DISCORD_TOKEN"))
+@bot.command()
+async def get_scoreboard(ctx):
+    result = get_scoreboard_short(league, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_projected_scoreboard(ctx):
+    result = get_projected_scoreboard(league, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_standings(ctx):
+    result = get_standings(league, top_half_scoring=False, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def top_half_wins(ctx):
+    result = top_half_wins(league, top_half_totals, week)
+    await ctx.send(result)
+
+@bot.command()
+async def get_projected_total(ctx):
+    result = get_projected_total(lineup)
+    await ctx.send(result)
+
+@bot.command()
+async def all_played(ctx):
+    result = all_played(lineup)
+    await ctx.send(result)
+
+@bot.command()
+async def get_monitor(ctx):
+    result = get_monitor(league)
+    await ctx.send(result)
+
+@bot.command()
+async def scan_roster(ctx):
+    result = scan_roster(lineup, team)
+    await ctx.send(result)
+
+@bot.command()
+async def get_matchups(ctx):
+    result = get_matchups(league, random_phrase=False, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_close_scores(ctx):
+    result = get_close_scores(league, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_waiver_report(ctx):
+    result = get_waiver_report(league, faab=False)
+    await ctx.send(result)
+
+@bot.command()
+async def get_power_rankings(ctx):
+    result = get_power_rankings(league, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_starter_counts(ctx):
+    result = get_starter_counts(league)
+    await ctx.send(result)
+
+@bot.command()
+async def best_flex(ctx):
+    result = best_flex(flexes, player_pool, num)
+    await ctx.send(result)
+
+@bot.command()
+async def optimal_lineup_score(ctx):
+    result = optimal_lineup_score(lineup, starter_counts)
+    await ctx.send(result)
+
+@bot.command()
+async def optimal_team_scores(ctx):
+    result = optimal_team_scores(league, week=None, full_report=False)
+    await ctx.send(result)
+
+@bot.command()
+async def get_achievers_trophy(ctx):
+    result = get_achievers_trophy(league, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_lucky_trophy(ctx):
+    result = get_lucky_trophy(league, week=None)
+    await ctx.send(result)
+
+@bot.command()
+async def get_trophies(ctx):
+    result = get_trophies(league, week=None)
+    await ctx.send(result)
+    
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+bot.run(os.environ.get("DISCORD_TOKEN"))
