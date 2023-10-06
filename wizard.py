5ec1ab16d24e93feeac5265cf1d4c1701096fbec
JasonRucinski <31479926+JasonRucinski@users.noreply.github.com>
2023-10-06 12:47:58 -0500

    Update wizard.py
    fixing

--- wizard.py
+++ wizard.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from FantasyFootballWizard.main import functions.py
+from functions import *
 import openai
 import os
 from dotenv import load_dotenv
@@ -27,97 +27,97 @@ from gamedaybot.espn.functionality import *
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command()
+@bot.command(aliases=['gs'])
 async def get_scoreboard(ctx):
     result = get_scoreboard_short(league, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_projected_scoreboard(ctx):
     result = get_projected_scoreboard(league, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_standings(ctx):
     result = get_standings(league, top_half_scoring=False, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def top_half_wins(ctx):
     result = top_half_wins(league, top_half_totals, week)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_projected_total(ctx):
     result = get_projected_total(lineup)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def all_played(ctx):
     result = all_played(lineup)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_monitor(ctx):
     result = get_monitor(league)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def scan_roster(ctx):
     result = scan_roster(lineup, team)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_matchups(ctx):
     result = get_matchups(league, random_phrase=False, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_close_scores(ctx):
     result = get_close_scores(league, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_waiver_report(ctx):
     result = get_waiver_report(league, faab=False)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_power_rankings(ctx):
     result = get_power_rankings(league, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_starter_counts(ctx):
     result = get_starter_counts(league)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def best_flex(ctx):
     result = best_flex(flexes, player_pool, num)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def optimal_lineup_score(ctx):
     result = optimal_lineup_score(lineup, starter_counts)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def optimal_team_scores(ctx):
     result = optimal_team_scores(league, week=None, full_report=False)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_achievers_trophy(ctx):
     result = get_achievers_trophy(league, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_lucky_trophy(ctx):
     result = get_lucky_trophy(league, week=None)
     await ctx.send(result)
 
-@bot.command()
+@bot.command(aliases=[])
 async def get_trophies(ctx):
     result = get_trophies(league, week=None)
     await ctx.send(result)
